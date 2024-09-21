eb9a286deda93774299c53ba2a4e1b9087f86135
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2024-09-21 21:34:37 +0800

    add function create slide

--- bot/handlers/presentation_handlers.py
+++ bot/handlers/presentation_handlers.py
@@ -4,6 +4,7 @@ from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import CallbackQuery, Message
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 router = Router()
 
@@ -22,9 +23,8 @@ async def create_presentation(callback: CallbackQuery, state: FSMContext):
 @router.message(PresentationSteps.first_step)
 async def get_images(message: Message, state: FSMContext):
     await state.set_state(PresentationSteps.images_step)
-    await message.answer(f'{message.photo}')
-    await message.bot.download(file=message.photo[-1].file_id, destination='images/1.jpg')
-    pprint(message.photo)
+    await message.bot.download(file=message.photo[-1].file_id, destination=f'images/{message.photo[-1].file_unique_id}.jpg')
+    await message.answer(f'Фото с номером {message.photo[-1].file_unique_id} сохранил')
 
 
 @router.message(PresentationSteps.images_step)
--- utils/create_presentation.py
+++ utils/create_presentation.py
@@ -1,19 +1,89 @@
 from pptx import Presentation
-from pptx.util import Inches
-import os
+from pptx.util import Inches, Pt
+from pptx.dml.color import RGBColor
+from pptx.enum.text import PP_ALIGN
+from pptx.enum.shapes import MSO_SHAPE
+
+from bot.settings import PATH_TO_0_IMG, PRESENTATION_PATH
 
 
-presentation = Presentation()
+def create_slide():
+    presentation_path = PRESENTATION_PATH
 
-slide_layout = presentation.slide_layouts[2]
+    """Открываем шаблон презентации"""
+    prs = Presentation(presentation_path)
 
-directory = "first slide"
-files = os.listdir(directory)
-slide = presentation.slides.add_slide(slide_layout)
+    """Добавляем слайд с макетом"""
+    slide_layout = prs.slide_layouts[6]
+    slide = prs.slides.add_slide(slide_layout)
+    shapes = slide.shapes
 
-for file in files:
-    img_path = f'first slide/{file}'
-    left = top = Inches(1)
-    slide.shapes.add_picture(img_path, left, top)
+    """Добавляем текст из сообщения вверху слайда"""
+    first_shape = shapes.add_textbox(
+        left=Inches(0.1),
+        top=Inches(0.1),
+        width=Inches(6),
+        height=Inches(0.47)
+    )
+    first_shape_frame = first_shape.text_frame
+    first_shape_frame.word_wrap = True
+    p = first_shape_frame.paragraphs[0]
+    p.text = "Текст из сообщения"  # изменяемое значение (текст из сообщения)
+    p.font.name = 'Newfont'
+    p.font.size = Pt(11)
+    p.font.color.rgb = RGBColor(190, 190, 190)
 
-presentation.save("test_presentation.pptx")
+    """Добавляем черный прямоугольник"""
+    second_shape = shapes.add_shape(
+        MSO_SHAPE.RECTANGLE,
+        left=Inches(0),
+        top=Inches(0.63),
+        width=prs.slide_width,
+        height=Inches(4.25)
+    )
+    fill_color = second_shape.fill
+    fill_color.solid()
+    fill_color.fore_color.rgb = RGBColor(0, 0, 0)
+    line = second_shape.line
+    line.color.rgb = RGBColor(0, 0, 0)
+
+    """Добавляем нулевую картинку"""
+    third_shape = shapes.add_picture(
+        PATH_TO_0_IMG,
+        left=Inches(1.45),
+        top=Inches(4.645),
+        width=Inches(8.45),
+        height=Inches(0.24)
+    )
+
+    """Добавляем текст "Освещенность, lx"""
+    fourth_shape = shapes.add_textbox(
+        left=Inches(0.2),
+        top=Inches(4.65),
+        width=Inches(1.25),
+        height=Inches(0.25)
+    )
+    fourth_shape_frame = fourth_shape.text_frame
+    first_shape_frame.word_wrap = True
+    p = fourth_shape_frame.paragraphs[0]
+    p.text = "Освещенность, lx"
+    p.font.size = Pt(10)
+    p.font.italic = True
+    p.font.color.rgb = RGBColor(190, 190, 190)
+
+    """Добавляем порядковый номер слайда"""
+    fifth_shape = shapes.add_textbox(
+        left=Inches(0.2),
+        top=Inches(4.65),
+        width=Inches(0.5),
+        height=Inches(0.75)
+    )
+    fifth_shape_frame = fifth_shape.text_frame
+    p = fifth_shape_frame.paragraphs[0]
+    p.text = "3"  # изменяемое значение (номер слайда)
+    p.font.size = Pt(60)
+    p.font.color.rgb = RGBColor(190, 190, 190)
+    p.alignment = PP_ALIGN.CENTER
+
+    """Сохраняем презентацию"""
+    prs.save(presentation_path)
