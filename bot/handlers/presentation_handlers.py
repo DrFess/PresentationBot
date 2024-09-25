5c88f9fd4636ee2f11a52acd3e584092c2cf1ffb
ALEKSEY <88752711+DrFess@users.noreply.github.com>
2024-09-25 20:02:37 +0800

    major update

--- bot/handlers/presentation_handlers.py
+++ bot/handlers/presentation_handlers.py
@@ -1,32 +1,47 @@
-from pprint import pprint
-
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import CallbackQuery, Message, InlineKeyboardButton, FSInputFile
 from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+from utils.create_presentation import create_slide
 
 router = Router()
 
 
 class PresentationSteps(StatesGroup):
-    first_step = State()
     images_step = State()
 
 
 @router.callback_query(F.data == 'create_presentation')
 async def create_presentation(callback: CallbackQuery, state: FSMContext):
-    await state.set_state(PresentationSteps.first_step)
-    await callback.message.answer('Отправь мне фото и описание для первого слайда')
-
-
-@router.message(PresentationSteps.first_step)
-async def get_images(message: Message, state: FSMContext):
+    await callback.message.answer('Отправь мне все фото для одного слайда и описание для этого слайда')
     await state.set_state(PresentationSteps.images_step)
-    await message.bot.download(file=message.photo[-1].file_id, destination=f'images/{message.photo[-1].file_unique_id}.jpg')
-    await message.answer(f'Фото с номером {message.photo[-1].file_unique_id} сохранил')
 
 
-@router.message(PresentationSteps.images_step)
-async def give_presentation(message: Message, state: FSMContext):
-    pass
+@router.message(F.photo, PresentationSteps.images_step)
+async def get_images(message: Message):
+    if message.caption is not None:
+        text = message.caption
+    else:
+        text = ''
+    await message.bot.download(
+        file=message.photo[-1].file_id,
+        destination=f'images/temp/{message.photo[-1].file_unique_id}.jpg'
+    )
+    create_slide(text=text)
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(text='Закончить и получить презентацию', callback_data='given_presentation'))
+    await message.answer(
+        'Слайд создан, для нового слайда также отправь все фото для него и подпись',
+        reply_markup=builder.as_markup()
+    )
+
+
+@router.callback_query(F.data == 'given_presentation', PresentationSteps.images_step)
+async def give_presentation(callback: CallbackQuery, state: FSMContext):
+    presentation_file = FSInputFile('/Users/aleksejdegtarev/PycharmProjects/PresentationBot/utils/given.pptx')
+    builder = InlineKeyboardBuilder()
+    builder.row(InlineKeyboardButton(text='Создать презентацию', callback_data='create_presentation'))
+    await callback.message.answer_document(document=presentation_file, reply_markup=builder.as_markup())
+    await state.clear()
--- utils/create_presentation.py
+++ utils/create_presentation.py
@@ -1,18 +1,21 @@
+import os
+
 from pptx import Presentation
 from pptx.util import Inches, Pt
 from pptx.dml.color import RGBColor
 from pptx.enum.text import PP_ALIGN
 from pptx.enum.shapes import MSO_SHAPE
 
-from bot.settings import PATH_TO_0_IMG, PRESENTATION_PATH
+from bot.settings import PRESENTATION_PATH, PATH_TO_0_IMG
 
 
-def create_slide():
+def create_slide(text: str) -> None:
     presentation_path = PRESENTATION_PATH
 
     """Открываем шаблон презентации"""
     prs = Presentation(presentation_path)
-
+    prs.save('given.pptx')
+    prs = Presentation('given.pptx')
     """Добавляем слайд с макетом"""
     slide_layout = prs.slide_layouts[6]
     slide = prs.slides.add_slide(slide_layout)
@@ -28,7 +31,7 @@ def create_slide():
     first_shape_frame = first_shape.text_frame
     first_shape_frame.word_wrap = True
     p = first_shape_frame.paragraphs[0]
-    p.text = "Текст из сообщения"  # изменяемое значение (текст из сообщения)
+    p.text = text  # изменяемое значение (текст из сообщения)
     p.font.name = 'Newfont'
     p.font.size = Pt(11)
     p.font.color.rgb = RGBColor(190, 190, 190)
@@ -85,5 +88,55 @@ def create_slide():
     p.font.color.rgb = RGBColor(190, 190, 190)
     p.alignment = PP_ALIGN.CENTER
 
+    """Добавляем 2 картинки"""
+    images = os.listdir('/Users/aleksejdegtarev/PycharmProjects/PresentationBot/bot/images/temp')
+    if len(images) == 2:
+        shapes.add_picture(
+            f'/Users/aleksejdegtarev/PycharmProjects/PresentationBot/bot/images/temp/{images[0]}',
+            left=Inches(0),
+            top=Inches(0.63),
+            width=prs.slide_width / 2,
+            height=Inches(4.02)
+        )
+        shapes.add_picture(
+            f'/Users/aleksejdegtarev/PycharmProjects/PresentationBot/bot/images/temp/{images[1]}',
+            left=prs.slide_width / 2,
+            top=Inches(0.63),
+            width=prs.slide_width / 2,
+            height=Inches(4.02)
+        )
+    elif len(images) == 4:
+        shapes.add_picture(
+            f'/Users/aleksejdegtarev/PycharmProjects/PresentationBot/bot/images/temp/{images[0]}',
+            left=Inches(0),
+            top=Inches(0.75),
+            width=Inches(3.5),
+            height=Inches(2.63)
+        )
+        shapes.add_picture(
+            f'/Users/aleksejdegtarev/PycharmProjects/PresentationBot/bot/images/temp/{images[1]}',
+            left=Inches(3.6),
+            top=Inches(0.75),
+            width=Inches(3.5),
+            height=Inches(2.63)
+        )
+        shapes.add_picture(
+            f'/Users/aleksejdegtarev/PycharmProjects/PresentationBot/bot/images/temp/{images[2]}',
+            left=Inches(7.2),
+            top=Inches(0.75),
+            width=Inches(2.46),
+            height=Inches(1.85)
+        )
+        shapes.add_picture(
+            f'/Users/aleksejdegtarev/PycharmProjects/PresentationBot/bot/images/temp/{images[3]}',
+            left=Inches(7.2),
+            top=Inches(2.7),
+            width=Inches(2.46),
+            height=Inches(1.85)
+        )
+
     """Сохраняем презентацию"""
-    prs.save(presentation_path)
+    prs.save('given.pptx')
+
+    for item in images:
+        os.remove(f'/Users/aleksejdegtarev/PycharmProjects/PresentationBot/bot/images/temp/{item}')
